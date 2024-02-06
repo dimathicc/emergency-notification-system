b0a87e763df3cc5190d85a00c9f7d819e1c8c700
Дмитрий Швалев <shvalevdd@mail.ru>
2024-02-06 13:13:08 +0300

    add api-gateway and api-test-service

---
+++ api-gateway/src/main/java/com/dimathicc/ens/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dimathicc.ens.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
---
+++ api-test-service/src/main/java/com/dimathicc/ens/apitestservice/ApiTestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dimathicc.ens.apitestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTestServiceApplication.class, args);
+	}
+
+}
